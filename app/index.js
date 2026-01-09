b5ae2df74a16ea720ecfe0e248d7cf1597b4c6a8
LucianoLupo95 <lnlupo95@gmail.com>
2026-01-09 11:26:31 +0100

    Test CI/CD 2

--- app/index.js
+++ app/index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.get("/health", (req, res) => {
-  res.json({ status: "ok", service: "node-express-test-api running" });
+  res.json({ status: "ok", service: "node-express-test-api running..." });
 });
 
 app.listen(PORT, "0.0.0.0", () => {
